d56a2534024e69b09b99233078556c6d419bfab0
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-13 08:42:53 -0300

    feat: add stopwatch start method

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine("");
+                Console.WriteLine(currentTime);
+                Console.WriteLine("");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
