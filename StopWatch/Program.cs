d3fa274c19b9fb23b01aff75462d4d0f119d582f
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-14 18:07:39 -0300

    feat: add initial structure to data validation

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -19,7 +19,6 @@
 
             return data;
         }
-        
         static void Menu()
         {
 
@@ -32,6 +31,41 @@
             Console.WriteLine(time);
             Console.WriteLine(type);
             Console.WriteLine("");
+        }
+
+        static void Validation(string data)
+        {
+            char type = data[^1];
+            string timePart = data.Substring(0, data.Length - 1);
+
+            if (string.IsNullOrEmpty(data) || (data != "0" && data.Length < 2))
+            {
+                Console.WriteLine("erro");
+                return;
+            }
+
+
+            if (type != 's' && type != 'm' && type != '0'  )
+            {
+                Console.WriteLine("erro");
+                return;
+            }
+
+            
+            if (!int.TryParse(timePart, out int time) || time < 0)
+            {
+                Console.WriteLine("erro");
+                return;
+            }
+        }
+
+        static string InvalidOption()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Opção inválida, por favor selecione uma opção válida: ");
+            Console.WriteLine("");
+            string data = MenuOptions();
+            return data;
         }
 
         static void Start()
